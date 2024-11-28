13eb1fa77ff8b6f775d3190ea393208de4dc4a19
Tahir Ishaq <tahirishaq10@gmail.com>
2024-11-28 17:47:42 +0000

    changes

---
+++ week-4/dags/etl.dag.py
@@ -0,0 +1,83 @@
+import os
+from airflow import DAG
+import user_funcs.ny_taxi as ny_taxi
+from airflow.utils.dates import days_ago
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
+
+BASE_PATH = os.getenv("HOME", "/home/airflow")
+
+DATASET_KEY = ""
+BUCKET_NAME = "b1-test"
+TABLE_NAME = "ny_green_taxi"
+DOWNLOAD_DATA_KEY = "downloaded_data"
+DOWNLOAD_DATA_TASK_ID = "download_data"
+LOAD_DATA_KEY = "load_data"
+LOAD_DATA_TASK_ID = "load_data"
+TRANSFORM_DATA_KEY = "transform_data"
+TRANSFORM_DATA_TASK_ID = "transform_data"
+CREATE_BUCKET_TASK_ID = "create_bucket"
+UPLOAD_TO_S3_TASK_ID = "upload_to_s3"
+UPLOAD_TO_DWH_TASK_ID = "upload_to_dwh"
+
+
+TAXI_YEAR = "2020"
+TAXI_MONTHS = ["10", "11", "12"]
+TAXI_COLOR = "green"
+BASE_URL = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/"
+URLS = [f"{BASE_URL}{TAXI_COLOR}_tripdata_{TAXI_YEAR}-{month}.csv.gz" for month in TAXI_MONTHS]
+
+default_args = {
+    "owner": "Tahir",
+	"start_date": days_ago(1),
+	"depends_on_past": False,
+	"retries": 0,
+    "provide_context": True
+}
+
+with DAG(
+	dag_id = "etl",
+	default_args = default_args,
+	# schedule_interval = "@once",
+    schedule_interval = None,
+	catchup = False,
+	max_active_runs = 1,
+	tags = ["etl"]
+) as dag:
+    download_data_from_url = PythonOperator(
+        task_id = DOWNLOAD_DATA_TASK_ID,
+        python_callable = ny_taxi.download_from_url,
+        op_kwargs = {"urls": URLS, "dir_path": BASE_PATH, "write_key": DOWNLOAD_DATA_KEY}
+    )
+    load_data = PythonOperator(
+        task_id = LOAD_DATA_TASK_ID,
+        python_callable = ny_taxi.load_data,
+        op_kwargs = {"dir_path": BASE_PATH, "read_key": DOWNLOAD_DATA_KEY, "task_ids": DOWNLOAD_DATA_TASK_ID, "write_key": LOAD_DATA_KEY}
+    )
+    transform_data = PythonOperator(
+        task_id = TRANSFORM_DATA_TASK_ID,
+        python_callable = ny_taxi.transform_data,
+        op_kwargs = {"dir_path": BASE_PATH, "read_key": LOAD_DATA_KEY, "write_key": TRANSFORM_DATA_KEY, "task_ids": LOAD_DATA_TASK_ID}
+    )
+    create_bucket = S3CreateBucketOperator(
+        task_id = CREATE_BUCKET_TASK_ID,
+        bucket_name = BUCKET_NAME
+    )
+    upload_to_s3 = PythonOperator(
+        task_id = UPLOAD_TO_S3_TASK_ID,
+        python_callable = ny_taxi.upload_s3,
+        op_kwargs = {"read_key": TRANSFORM_DATA_KEY, "task_ids": TRANSFORM_DATA_TASK_ID, "bucket_name": BUCKET_NAME, "table_name": TABLE_NAME}
+    )
+    upload_to_dwh = PythonOperator(
+        task_id = UPLOAD_TO_DWH_TASK_ID,
+        python_callable = ny_taxi.upload_dwh,
+        op_kwargs = {"read_key": TRANSFORM_DATA_KEY, "task_ids": TRANSFORM_DATA_TASK_ID, "table_name": TABLE_NAME}
+    )
+    # create_local_to_s3_job = LocalFilesystemToS3Operator(
+    #     task_id="create_local_to_s3_job",
+    #     filename=f"{base_path}/sample.txt",
+    #     dest_key="sample.txt",
+    #     dest_bucket="b1-test",
+    #     replace=True,
+    # )
+    download_data_from_url >> load_data >> transform_data >> create_bucket >> [upload_to_s3, upload_to_dwh]
---
+++ week-4/dags/user_funcs/ny_taxi.py
@@ -0,0 +1,143 @@
+"""
+Functions related to ny taxi dataset
+"""
+import os
+import re
+import user_funcs.data_types as data_types
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from sqlalchemy import create_engine
+
+
+def get_file_name_from_url(url):
+    """Returns file name from URL"""
+    name_idx_st = len(url) - url[::-1].find("/")
+    file_name = url[name_idx_st:]
+    return file_name
+
+
+def download_from_url(urls, dir_path, write_key, **kwargs):
+    """
+    Download file from URL
+    urls(list): A list of URLs
+    **kwargs: Airflow classes/functions/variables
+    """
+    file_names = [] # Contains the absolute path
+    for url in urls:
+        file_name = f"{dir_path}/{get_file_name_from_url(url)}"
+        os.system(f"wget {url} -O {file_name}")
+        file_names.append(file_name)
+    kwargs["ti"].xcom_push(key=write_key, value=file_names)
+
+
+def load_data_test(dir_path, read_key, task_ids, write_key, **kwargs):
+    """Load the dataset, combined it and save it locally and push the name of the dataset in xcom"""
+    load_val = kwargs["ti"].xcom_pull(key=read_key, task_ids=task_ids)
+    print("The type of all_datasets")
+    print(type(load_val))
+    print("The loaded value from xcom")
+    print(load_val)
+
+
+def load_data(dir_path, read_key, task_ids, write_key, **kwargs):
+    """Load the dataset, combined it and save it locally and push the name of the dataset in xcom"""
+    all_datasets = kwargs["ti"].xcom_pull(key=read_key, task_ids=task_ids)
+    print("The type of all_datasets")
+    print(type(all_datasets))
+    print("The value of all_datasets")
+    print(all_datasets)
+    for idx, dataset in enumerate(all_datasets):
+        print("The value of dataset")
+        print(dataset)
+        if idx == 0:
+            df = pd.read_csv(dataset, dtype=data_types.green_taxi_other_col, parse_dates=data_types.green_taxi_date_col, nrows=0)
+        new_df = pd.read_csv(dataset, dtype=data_types.green_taxi_other_col, parse_dates=data_types.green_taxi_date_col)
+        df = pd.concat([df, new_df])
+    df_name = f"{dir_path}/combined_dataset.parquet"
+    df.to_parquet(df_name)
+    # write_key = "combined_dataset"
+    print(df_name)
+    kwargs["ti"].xcom_push(key=write_key, value=df_name)
+
+
+def camel_to_snake(name):
+    """
+    Converts camel case string to snake case
+    Copied from https://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-snake-case#1176023
+    """
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+
+def transform_data(dir_path, read_key, write_key, task_ids, **kwargs):
+    """
+    Transform and return the updated dataframe
+    All columns names should be snake case
+    A new col Date column needs to added
+    """
+    dataset = kwargs["ti"].xcom_pull(key=read_key, task_ids=task_ids)
+    df = pd.read_parquet(dataset)
+    df.columns = [camel_to_snake(col_name) for col_name in df.columns]
+    df = df[df["passenger_count"] > 0]
+    df = df[df["trip_distance"] > 0]
+    df = df[df["vendor_id"].isin(df["vendor_id"].unique())]
+    df["lpep_pickup_date"] = df["lpep_pickup_datetime"].dt.date
+    print(pd.io.sql.get_schema(df, name="schema_test"))
+    df_name = f"{dir_path}/transformed_dataset.parquet"
+    df.to_parquet(df_name)
+    os.environ[write_key] = df_name
+    kwargs["ti"].xcom_push(key=write_key, value=df_name)
+
+
+def upload_dwh(read_key, task_ids, table_name, **kwargs):
+    """Upload pd.df data to dwh"""
+    dataset = kwargs["ti"].xcom_pull(key=read_key, task_ids=task_ids)
+    df = pd.read_parquet(dataset)
+    print(pd.io.sql.get_schema(df, name="schema_test"))
+    username = os.getenv("DWH_USERNAME")
+    password = os.getenv("DWH_PASSWORD")
+    host = os.getenv("DWH_HOST")
+    port = os.getenv("DWH_PORT")
+    db_name = os.getenv("DWH_DB_NAME")
+    db_login = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}"
+    print("postgres db_login string")
+    print(db_login)
+    # engine = create_engine(db_login)
+    with create_engine(db_login).connect() as con:
+        df.head(n=0).to_sql(name=table_name, con=con, schema="bronze", if_exists="replace")
+        df.to_sql(name=table_name, con=con, schema="bronze", if_exists="append")
+
+
+def upload_s3(read_key, task_ids, bucket_name, table_name, **kwargs):
+    """Upload data to S3"""
+    file_name = kwargs["ti"].xcom_pull(key=read_key, task_ids=task_ids)
+    table = pq.read_table(file_name)
+    root_path = f"{bucket_name}/{table_name}"
+    s3 = pa.fs.S3FileSystem(scheme="http", endpoint_override="s3:9000")
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols=["lpep_pickup_date"],
+        filesystem=s3
+    )
+
+
+def upload_dwh_alt(read_key, table_name, **kwargs):
+    """Upload pd.df data to dwh"""
+    dataset = os.getenv(read_key)
+    print(f"The value of {read_key} is: {dataset}")
+    df = pd.read_parquet(dataset)
+    username = os.getenv("DWH_USERNAME")
+    password = os.getenv("DWH_PASSWORD")
+    # password = "dwh_pass"
+    host = os.getenv("DWH_HOST")
+    port = os.getenv("DWH_PORT")
+    db_name = os.getenv("DWH_DB_NAME")
+    db_login = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}"
+    print("postgres db_login string")
+    print(db_login)
+    # engine = create_engine(db_login)
+    with create_engine(db_login).connect() as con:
+        df.head(n=0).to_sql(name=table_name, con=con.connection, if_exists="replace")
+        df.to_sql(name=table_name, con=con, schema="bronze", if_exists="append")
